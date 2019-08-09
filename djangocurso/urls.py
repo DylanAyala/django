8f73bfd30026672f9d2a1e96f8e8567623d26e1a
dayala <dylan.ayala@sondeos.com.ar>
2019-08-09 17:00:48 -0300

    se sigue

--- djangocurso/urls.py
+++ djangocurso/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include(('modules.bases.urls', 'bases'), namespace='bases')),
+    path('inv/', include(('modules.inv.urls', 'inv'), namespace='inv')),
     path('admin/', admin.site.urls),
 ]
